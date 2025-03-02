f38dcf52693f22450578425ef1f63f0111da236e
mosestyle <76072822+mosestyle@users.noreply.github.com>
2025-03-02 19:34:15 +0100

    Updates

---
+++ precache-manifest.5237728e16974c1641091fa76eb2d81a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74a3698d08f5378259a30a03cec8c12",
+    "url": "/color-studio/index.html"
+  },
+  {
+    "revision": "785c115afe36c8935a29",
+    "url": "/color-studio/static/css/main.361bfbed.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/color-studio/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/color-studio/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "785c115afe36c8935a29",
+    "url": "/color-studio/static/js/main.c3f8cad3.chunk.js"
+  },
+  {
+    "revision": "9d4ef1367fec0d0da162",
+    "url": "/color-studio/static/js/runtime~main.49327c20.js"
+  }
+]);
